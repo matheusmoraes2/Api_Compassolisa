afd416cf3d98e8509911518d9915332fef64ea30
matheusmoraes2 <matheusmoraes85@hotmail.com>
2022-02-04 11:27:31 -0300

    fix: refazendo validation de objectID

--- src/app/validation/Car/objectID.js
+++ src/app/validation/Car/objectID.js
@@ -1,11 +1,16 @@
-const ObjectId = require('mongoose').Types.ObjectId;
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
-module.exports = async (req,res,next) =>{
-  try {
-    if(!(ObjectId.isValid(req.params.id))) throw new Error();
+module.exports = async (req,res,next) => {
+  try{
+    const CarSchema = Joi.object({
+      id: Joi.objectId()
+    }); 
+
+    const {error} = await CarSchema.validate(req.params,{abortEarl:true});
+    if(error) throw error ;
     return next();
-  } catch (error) {
-    return res.status(400).json(`ObjectId:'${req.params.id}' is invalid`);
-                
+  }catch(error){
+    return res.status(400).json(error.message);
   }
 };
